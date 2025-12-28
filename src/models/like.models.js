54da330c1361d7567d6c42be331e24ea178df50d
Eshita Kapat <eshita.kapat@outlook.com>
2025-12-28 14:02:47 +0530

    Initial commit

---
+++ src/models/like.models.js
@@ -0,0 +1,38 @@
+ /*
+ comment ObjectId comments
+  createdAt Date
+  video ObjectId videos
+  updatedAt Date
+  likedBy ObjectId users
+  tweet ObjectId tweets
+  */
+
+  import mongoose , {Schema} from "mongoose";
+
+  const likeSchema = new Schema ({
+
+ video:{
+    type: Schema.Types.ObjectId,
+    ref:"Video"
+ },
+ comment:{
+    type:Schema.Types.ObjectId,
+    ref:"Comment",
+ },
+ tweet: {
+    type: Schema.Types.ObjectId,
+    ref:"Tweet"
+ },
+ likedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+ },
+
+  },
+
+{timestamps: true}
+
+);
+
+export const Like = mongoose.model("Like" ,  likeSchema)
+
